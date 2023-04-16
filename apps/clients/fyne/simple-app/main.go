754e9c0321dc75b2767880d284e5b748e3427b64
Sean Knowles <s@seanknowles.dev>
2023-04-16 20:23:55 +0100

    feat: added theme selection drop down

--- apps/clients/fyne/simple-app/main.go
+++ apps/clients/fyne/simple-app/main.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 type App struct {
-	output *widget.Label
+    output *widget.Label
+    dark   bool
 }
 
 var myApp App
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+    a := app.New()
+    w := a.NewWindow("Hello World")
 
-	output, entry, btn := myApp.makeUI()
+    output, entry, buttons := myApp.makeUI()
 
-	w.SetContent(container.NewVBox(output, entry, btn))
-	w.ShowAndRun()
+    w.SetContent(container.NewVBox(output, entry, buttons))
+    w.Resize(fyne.NewSize(400, 200))
+    w.ShowAndRun()
 }
 
-func (app *App) makeUI (*widget.Label, *widget.Entry, *widget.Button) {
-	output := widget.NewLabel("Hello World!")
-	entry := widget.NewEntry()
-	btn := widget.NewButton("Enter", func() {
-		app.output.SetText(entry.Text)
-	}, )
+func (app *App) makeUI() (*widget.Label, *widget.Entry, *fyne.Container) {
+    output := widget.NewLabel("Hello World!")
+    entry := widget.NewEntry()
 
-	app.output = output
+    enterBtn := widget.NewButton("Enter", func() {
+        app.output.SetText(entry.Text)
+    })
 
+    enterBtn.Importance = widget.HighImportance
 
-	return output, entry, btn
+    selectOptions := []string{"Light Theme", "Dark Theme"}
+    selectWidget := widget.NewSelect(selectOptions, func(selected string) {
+        if selected == "Dark Theme" {
+            app.dark = true
+            fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
+        } else {
+            app.dark = false
+            fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
+        }
+    })
+	selectWidget.PlaceHolder = "Select Theme"
+
+    buttons := container.NewHBox(enterBtn, selectWidget)
+
+    app.output = output
+
+    return output, entry, buttons
 }
-
-
