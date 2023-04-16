97484c853497a77f084c93ab81473b4a3182a1b5
Sean Knowles <s@seanknowles.dev>
2023-04-16 19:14:32 +0100

    feat: hello world setup and working :)

--- apps/clients/fyne/simple-app/main.go
+++ apps/clients/fyne/simple-app/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func Hello(name string) string {
 	result := "Hello " + name
@@ -9,4 +14,9 @@ func Hello(name string) string {
 
 func main() {
 	fmt.Println(Hello("clients-fyne-simple-app"))
+	a := app.New()
+
+	w := a.NewWindow("Hello World")
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
