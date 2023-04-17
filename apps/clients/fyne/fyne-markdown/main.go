d7b82c7a3777bc0230d507d8219d17f5ffec6a01
Sean Knowles <s@seanknowles.dev>
2023-04-17 11:37:44 +0100

    feat: added fyne-markdown

--- apps/clients/fyne/fyne-markdown/main.go
+++ apps/clients/fyne/fyne-markdown/main.go
@@ -1,12 +1,100 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
+// config holds information that we can share with any function
+// that has a receiver of type *config
+type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
 }
 
+var cfg config
+
 func main() {
-	fmt.Println(Hello("clients-fyne-fyne-markdown"))
+	// create a fyne app
+	a := app.New()
+
+	// create a window for the app
+	win := a.NewWindow("Markdown")
+
+	// get the user interface
+	edit, preview := cfg.makeUI()
+	cfg.createMenuItems(win)
+
+	// set the content of the window
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	// show window and run app
+	win.Resize(fyne.Size{Width: 800, Height: 500})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+// makeUI creates two widgets, assigns them to the app config, and
+// adds a listener on the edit widget that updates the preview widget
+// with parsed markdown whenever the user types something
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+
+	edit.OnChanged = preview.ParseMarkdown
+
+	return edit, preview
+}
+
+func (app *config) createMenuItems(win fyne.Window) {
+
+	// create three menu items
+	openMenuItem := fyne.NewMenuItem("Open...", func() {})
+	saveMenuItem := fyne.NewMenuItem("Save", func() {})
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", app.saveAsFunc(win))
+
+	// create a file menu, and add the three items to it
+	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
+
+	// create a main menu, and add the file menu to it
+	menu := fyne.NewMainMenu(fileMenu)
+
+	// set the main menu for the application
+	win.SetMainMenu(menu)
+}
+
+func (app *config) saveAsFunc(win fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(write fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			if write == nil {
+				// user cancelled
+				return
+			}
+
+			// save file
+			write.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = write.URI()
+
+			defer write.Close()
+
+			win.SetTitle(win.Title() + " - " + write.URI().Name())
+			app.SaveMenuItem.Disabled = false
+
+		}, win)
+		saveDialog.Show()
+	}
 }
